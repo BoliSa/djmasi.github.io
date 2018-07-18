b5510152c2cf751c59ab2cce7482a6f43cba6b33
djmasi <sarah.boliviani@web.de>
2018-07-18 08:59:45 +0200

    Initial project commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function PlaySound() {
+    var path = "media/"
+    var snd = new Audio(path+"pizza.mp3");
+    snd.play(); 
+}
+
+function PlaySound2() {
+    var path = "media/"
+    var snd = new Audio(path+"booh.mp3");
+    snd.play(); 
+}        
+
+function PlaySound3() {
+    var path = "media/"
+    var snd = new Audio(path+"mah.mp3");
+    snd.play(); 
+}
+
+function PlaySound4() {
+    var path = "media/"
+    var snd = new Audio(path+"booh.mp3");
+    snd.play(); 
+}
+
+function PlaySound5() {
+    var path = "media/"
+    var snd = new Audio(path+"pizza.mp3");
+    snd.play(); 
+}
+
+function PlaySound6() {
+    var path = "media/"
+    var snd = new Audio(path+"no5.mp3");
+    snd.play();
+}
